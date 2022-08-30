70bf2a8a41497a917151a369d8789e3c163f6a1b
inf0craw1 <inf0craw1@naver.com>
2022-08-30 17:41:26 +0900

    feat: add 10171 10430 18108 2588 3003

---
+++ baekjoon/3003.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define debug if constexpr (local) std::cout
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+ 
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+/* ----------------------------------------------- */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
+
+    int numbers[] = {1, 1, 2, 2, 2, 8};
+    int tmp;
+
+
+    for(int i = 0; i < 6; i++) {
+        cin >> tmp;
+        numbers[i] -= tmp;
+        cout << numbers[i] << ' ';
+    }
+    cout << endl;
+    return 0;
+}
