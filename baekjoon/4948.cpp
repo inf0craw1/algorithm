79873bd5e7ecb94938117c88a3f812ebae43fb52
inf0craw1 <inf0craw1@naver.com>
2022-09-18 15:01:18 +0900

    solve

---
+++ baekjoon/4948.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#define debug if constexpr (local) std::cout
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+ 
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+/* ----------------------------------------------- */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
+
+	int n, res;
+	bool era[1000001] = {true, true, };
+	
+	for ( int i = 2; i <= 1000; i++ ) {
+		if ( era[i] ) continue;
+		for (int j = i * 2; j <= 1000000; j += i ) {
+			era[j] = true;
+		}
+	}
+	
+	while ( true  ) {
+		cin >> n;
+		if ( n == 0 ) break;
+		res = 0;
+
+		for ( int i = n + 1; i <= 2 * n ; i++ ) {
+			if ( !era[i] ) res ++;
+		}
+		cout << res << endl;
+	}
+
+    return 0;
+}
