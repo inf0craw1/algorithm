7b949d920269c40e15a2599f4b3d8eed7c92b9fb
inf0craw1 <inf0craw1@naver.com>
2023-03-27 16:00:54 +0900

    solve: 1085

---
+++ baekjoon/1085.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#define debug if constexpr (local) std::cout
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+ 
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+/* ----------------------------------------------- */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
+
+	int x, y, w, h, mini;
+
+	cin >> x >> y >> w >> h;
+
+	mini = min(x, y);
+	mini = min(mini, w - x);
+	mini = min(mini, h - y);
+
+	cout << mini << endl;
+
+    return 0;
+}
