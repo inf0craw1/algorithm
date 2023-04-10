fd030dfdd9ef37cf9b565e07ff28fae03d7c1bed
inf0craw1 <inf0craw1@naver.com>
2023-04-10 15:50:27 +0900

    solve: 1764

---
+++ baekjoon/1764.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+#define debug if constexpr (local) std::cout
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+ 
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+/* ----------------------------------------------- */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
+
+	
+	int m, n, counter = 0;
+	set<string> s;
+	vector<string> v;
+
+	cin >> m >> n;
+
+	for ( int i = 0; i < m; i++ ) {
+		string temp;
+
+		cin >> temp;
+
+		s.insert(temp);
+
+	}
+
+	for ( int i = 0; i < n; i++ ) {
+
+		string temp;
+
+		cin >> temp;
+
+		if ( s.find(temp) != s.end() ) {
+			v.push_back(temp);
+			counter ++;
+		}
+	}
+
+	sort(all(v));
+
+	cout << counter << endl;
+	for ( auto vv: v ) {
+		cout << vv << endl;
+	}
+
+    return 0;
+}
