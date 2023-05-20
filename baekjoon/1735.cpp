7f72c374aeaa60d7649721f71c4b060acda9f540
inf0craw1 <inf0craw1@naver.com>
2023-05-20 18:01:04 +0900

    not solved

--- baekjoon/1735.cpp
+++ baekjoon/1735.cpp
@@ -20,84 +20,30 @@ using namespace std;
 
 /* - GLOBAL VARIABLES ---------------------------- */
 
+int n[2], d[2];
+
 /* ----------------------------------------------- */
 
 /* - FUNCTIONS ----------------------------------- */
+
+void inputs() {
+	cin >> n[0] >> d[0];
+	cin >> n[1] >> d[1];
+}
+
 /* ----------------------------------------------- */
 
 int main() {
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
     if constexpr (local) 
-        (void)!freopen("input.txt", "r", stdin);
+    	(void)!freopen("input.txt", "r", stdin);
 
-	int a, b, c, d;
-	int resNumerator, resDenominator;
-	vector<bool> sieveOfEratosthenes(175, false);
-	vector<int> primeNumbers;
-	int tempCommon = 1, common; 
+	inputs();
 
-	cin >> a >> b >> c >> d;
-
-	for ( int i = 2; i < 175; i++ ) {
-		if ( !sieveOfEratosthenes[i] ) {
-			primeNumbers.push_back(i);
-
-			for ( int j = i; j < 175; j += i  ) {
-				sieveOfEratosthenes[j] = true;
-			}
-		}
-	}
-
-
-	while ( 1 ) {
-		bool chk = false;
-		for ( int i = 0; primeNumbers[i] < min(b, d); i++ ) {
-			if ( b % primeNumbers[i] == 0 && d % primeNumbers[i] == 0 ) {
-				tempCommon *= primeNumbers[i];
-
-				b /= primeNumbers[i];
-				d /= primeNumbers[i];
-
-				chk = true;
-			}
-		}
-
-		if ( !chk ) {
-			break;
-		}
-	}
-	
-
-	common = tempCommon * b * d;
-	
-	a *= common / b;
-	c *= common / d;
-
-
-	resNumerator = a + c;
-	resDenominator = common;
-
-	common = 1;
-	
-	while (1) {
-		bool chk = false;
-		for ( int i = 0; primeNumbers[i] < resDenominator; i++ ) {
-			if ( resNumerator % primeNumbers[i] == 0 && resDenominator % primeNumbers[i] == 0 ) {
-				common *= primeNumbers[i];
-				resNumerator /= primeNumbers[i];
-				resDenominator /= primeNumbers[i];
-				chk = true;
-			}
-		}
-		if ( !chk ) {
-			break;
-		}
-	}
-	
-
-	cout << resNumerator << ' '  << resDenominator << endl;
+	cout << n[0];
+	cout << d[1];
 
 
 
