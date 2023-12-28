5ba1301ce5eb3f811654041b91194a8da8122abf
inf0craw1 <inf0craw1@naver.com>
2023-12-28 19:00:39 +0900

    solved

---
+++ baekjoon/15657.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define debug if constexpr (local) std::cout
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+
+int n, m;
+vector<int> arr;
+ 
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+void sequence(vector<int> v) {
+	if ( v.size() != 0 ) {
+		if ( v.size() >= m ) {
+			for ( auto vv:v ) {
+				cout << arr[vv-1] << ' ';
+			}
+			cout << endl;
+			return;
+		}
+	}
+	int startNumber = 1;
+	if ( v.size() != 0 ) {
+		startNumber = v[v.size() - 1];
+	}
+	for ( int i = startNumber; i <= n; i++ ) {
+		vector<int> newVec = v;
+		newVec.push_back(i);
+		sequence(newVec);
+	}
+	return;
+}
+
+/* ----------------------------------------------- */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
+
+	int temp;
+
+	cin >> n >> m;
+
+	for( int i = 0; i < n ; i++ ) {
+		cin >> temp;
+		arr.push_back(temp);
+	}
+	sort(arr.begin(), arr.end());
+
+	vector<int> empty;
+
+	sequence(empty);
+
+    return 0;
+}
