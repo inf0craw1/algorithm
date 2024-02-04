d1169324b7af54ef4698106120c3b0dcb728e028
inf0craw1 <inf0craw1@naver.com>
2024-02-04 22:14:40 +0900

    solved

--- src/9519/main.cpp
+++ src/9519/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 
 #define debug if constexpr (local) std::cout
 #define endl '\n'
@@ -18,11 +20,33 @@ typedef unsigned long long ull;
 using namespace std;
 
 /* - GLOBAL VARIABLES ---------------------------- */
- 
+int blinking, len, mixCount, cycle = -1;
+vector<char> existingStr, str;
+vector<string> blinkingStrs;
+string str1;
 /* ----------------------------------------------- */
 
 /* - FUNCTIONS ----------------------------------- */
+bool IsSameVector(vector<char> v1, vector<char> v2) {
+	int l = v1.size();
+	for ( int i = 0; i < l; i++ ) {
+		if ( v1[i] != v2[i] ) return 0;
+	}
+	return 1;
+}
+void DoBlink() {
+	int i;
+	vector<char> res;
 
+	for ( i = 0; i < len; i+=2 ) {
+		res.push_back(str[i]);
+	}
+	i -= ( len % 2 == 0 ) ? 1 : 3;
+	for (; i > 0; i-=2 ) {
+		res.push_back(str[i]);
+	}
+	str = res;
+}
 /* ----------------------------------------------- */
 
 int main() {
@@ -32,5 +56,40 @@ int main() {
     if constexpr (local) 
         (void)!freopen("input.txt", "r", stdin);
 
+	cin >> blinking;
+
+	cin >> str1;
+
+	blinkingStrs.push_back(str1);
+	existingStr = vector<char> (str1.begin(), str1.end());
+	str = existingStr;
+	len = existingStr.size();
+
+	if ( len <= 2 ) {
+		cout << str1 << endl;
+		return 0;
+	}
+
+	mixCount = ((len % 2 == 0) ? (len/2 - 1) : (len/2));
+
+	for ( int i = 0; i < blinking; i++ ) {
+		DoBlink();
+		blinkingStrs.push_back(string(str.begin(), str.end()));
+
+		if ( IsSameVector(existingStr, str) ) {
+			cycle = ++i;
+			break;
+		}
+
+	}
+	if ( cycle == -1 ) {
+		for ( auto s: str ) {
+			cout << s;
+		}
+		cout << endl;
+		return 0;
+	}
+
+	cout << blinkingStrs[blinking%cycle] << endl;
     return 0;
 }
