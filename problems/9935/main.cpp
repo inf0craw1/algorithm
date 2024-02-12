66d493a6fab092847d7a6450498148bf32781241
inf0craw1 <inf0craw1@naver.com>
2024-02-13 02:41:38 +0900

    solved

--- problems/9935/main.cpp
+++ problems/9935/main.cpp
@@ -22,33 +22,13 @@ typedef pair<ll, ll> pi;
 typedef unsigned long long ull;
 
 /* - GLOBAL VARIABLES ---------------------------- */
-string str, exStr;
 int len, exLen;
+vector<char> stck;
+string str, exStr;
 /* ----------------------------------------------- */
 
 /* - FUNCTIONS ----------------------------------- */
-pi FindChainingExplosion(int s, int from, int jump) {
 
-	for ( int i = s; i < from ; i++ ) {
-		bool chkFound = true;
-		for ( int j = i, k = 0; j < len && k < exLen; j++, k++ ) {
-			if ( j == from ) {
-				j += jump;
-				k--;
-				continue;
-			}
-			if ( str[j] != exStr[k] ) {
-				chkFound = false;
-				break;
-			} 
-		}
-		if ( chkFound ) {
-			pi res = FindChainingExplosion(max(0, i - exLen + 1), i, jump + exLen);
-			return res;
-		}
-	}
-	return make_pair(from, jump);
-}
 /* ----------------------------------------------- */
 
 int main() {
@@ -59,49 +39,38 @@ int main() {
         (void)!freopen("input.txt", "r", stdin);
 
 	cin >> str >> exStr;
+
 	len = str.size();
 	exLen = exStr.size();
-
-	while ( 1 ) {
-		int start = 0;
-		vector<pi> skipParts;
-
-		while ( start < len ) {
-			auto f = str.find(exStr, start);
-			if ( f == string::npos ) {
-				break;
-			}
-			pi res = FindChainingExplosion( max(0, (int)f - exLen + 1), f, exLen - 1);
-			start = res.fi + res.se;
-			skipParts.push_back(res);
-		}
-
-		string resStr = "";
 	
-		for ( int i = 0; i < len; i++ ) {
-			bool isSkipped = false;
-			for ( auto s: skipParts ) {
-				if ( i == s.fi ) {
-					isSkipped = true;
-					i += s.se;
+	for ( int i = 0; i < len; i++ ) {
+		stck.push_back(str[i]);
+		
+		if ( stck.size() >= exLen ) {
+			bool isMatch = true;
+			for ( int j = stck.size() - 1, k = exLen - 1; k >= 0 ; j--, k-- ) {
+				if ( stck[j] != exStr[k] ) {
+					isMatch = false;
 					break;
 				}
 			}
-			if ( isSkipped ) continue;
-			
-			resStr += str[i];
+			if ( isMatch ) {
+				for ( int j = 0; j < exLen; j++ ) {
+					stck.pop_back();
+				}
+			}
 		}
-		str = resStr;
-		len = str.size();
-
-		if ( skipParts.empty() ) break;
 	}
-
-	if ( str == "" ) {
+	
+	if ( stck.empty() ) {
 		cout << "FRULA" << endl;
 		return 0;
 	}
-	cout << str << endl;
+
+	for ( auto ss: stck ) {
+		cout << ss;
+	}
+	cout << endl;
 
     return 0;
 }
