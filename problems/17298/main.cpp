679a40dd5777f820eb28ea087b2160301628dfeb
inf0craw1 <inf0craw1@naver.com>
2024-02-13 02:41:43 +0900

    not solved

---
+++ problems/17298/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define debug if constexpr (local) std::cout
+#define endl '\n'
+#define inf 0x3f3f3f3f
+#define linf 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+using namespace std;
+
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> pi;
+typedef unsigned long long ull;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+/* ----------------------------------------------- */
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
+
+    return 0;
+}
