7f84bc635c3bdc40c496c0f93decd477fb38e2bc
inf0craw1 <inf0craw1@naver.com>
2024-02-21 03:10:11 +0900

    solved

--- problems/7662/main.cpp
+++ problems/7662/main.cpp
@@ -1,98 +1,109 @@
 #include <bits/stdc++.h>
 
+#define debug if constexpr (local) std::cout
 #define endl '\n'
+#define inf 0x3f3f3f3f
 #define linf 0x3f3f3f3f3f3f3f3f
 #define fi first
 #define se second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
 using namespace std;
 
+#ifdef LOCAL
+constexpr bool local = true;
+#else
+constexpr bool local = false;
+#endif
+
 typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> pi;
+typedef unsigned long long ull;
 
 /* - GLOBAL VARIABLES ---------------------------- */
-priority_queue<int> maxQ, minQ;
-unordered_map<int, int> maxDeletedList, minDeletedList;
 /* ----------------------------------------------- */
 
 /* - FUNCTIONS ----------------------------------- */
-void PushDeletedList(int target, int type) {
-	if ( type == 1 ) {
-		auto foundMap = maxDeletedList.find(target);
-		if ( foundMap == maxDeletedList.end() ) {
-			maxDeletedList.insert({target, 1});
-			return;
-		}
-		foundMap->se ++;
-		return;
-	}
-	auto foundMap = minDeletedList.find(target);
-	if ( foundMap == minDeletedList.end() ) {
-		minDeletedList.insert({target, 1});
-		return;
-	}
-	foundMap->se ++;
-	return;
-}
-ll PopQueue(int dir) {
-	if ( dir == 1 ) {
-		while ( maxQ.size() ) {
-			int popNum = maxQ.top();
-			maxQ.pop();
-			auto foundPopNum = maxDeletedList.find(popNum);
 
-			if ( foundPopNum == maxDeletedList.end() || foundPopNum->se <= 0 ) {
-				return popNum;
-			}
-			foundPopNum->se --;
-		}
-		return linf;
-	}
-	while ( minQ.size() ) {
-		int popNum = minQ.top() * -1;
-		minQ.pop();
-		auto foundPopNum = minDeletedList.find(popNum);
-
-		if ( foundPopNum == minDeletedList.end() || foundPopNum->se <= 0 ) {
-			return popNum;
-		}
-		foundPopNum->se --;
-	}
-	return linf;
-}
 /* ----------------------------------------------- */
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
-	int cases; cin >> cases;
+    if constexpr (local) 
+        (void)!freopen("input.txt", "r", stdin);
 
-	for ( int i = 0; i < cases; i++ ) {
-		maxQ = priority_queue<int>();
-		minQ = priority_queue<int>();
-		maxDeletedList.clear();
-		minDeletedList.clear();
-		int numberOfCommands; cin >> numberOfCommands;
+	int testCases; cin >> testCases;
 
-		for ( int i = 0; i < numberOfCommands; i++ ) {
+
+	for ( int z = 0; z < testCases; z++ ) {
+		priority_queue<ll> maxQ, minQ;
+		unordered_map<ll,ll> nMap;
+		int commands; cin >> commands;
+		
+		for ( int k = 0; k < commands; k++ ) {
 			char command; cin >> command;
-			int curNum; cin >> curNum;
+			ll n; cin >> n;
+
 			if ( command == 'I' ) {
-				maxQ.push(curNum);
-				minQ.push(curNum * -1);
+				maxQ.push(n);
+				minQ.push(n * -1);
+				auto nFound = nMap.find(n);
+				if ( nFound == nMap.end() ) {
+					nMap.insert({n, 1});
+					continue;
+				}
+				nFound->se ++;
 				continue;
 			}
-			ll res = PopQueue(curNum);
-			if ( res != linf ) {
-				PushDeletedList(res, curNum * -1);
+
+			if ( n == 1 ) {
+				while ( maxQ.size() ) {
+					ll curTop = maxQ.top();
+					maxQ.pop();
+					auto nFound = nMap.find(curTop);
+					if ( nFound->se == 0 ) continue;
+					nFound->se --;
+					break;
+				}
+				continue;
+			}
+			while ( minQ.size() ) {
+				ll curTop = minQ.top() * -1;
+				minQ.pop();
+				auto nFound = nMap.find(curTop);
+				if ( nFound->se == 0 ) continue;
+				nFound->se --;
+				break;
 			}
 		}
-		ll res1 = PopQueue(1);
-		ll res2 = PopQueue(-1);
-		if ( res1 == linf || res2 == linf ) {
+
+		ll maxi, isExist = 0;
+		while ( maxQ.size() ) {
+			ll curTop = maxQ.top();
+			maxQ.pop();
+			auto nFound = nMap.find(curTop);
+			if ( nFound->se == 0 ) continue;
+			maxi = nFound->fi;
+			isExist = 1;
+			break;
+		}
+		ll mini;
+		while ( minQ.size() ) {
+			ll curTop = minQ.top() * -1;
+			minQ.pop();
+			auto nFound = nMap.find(curTop);
+			if ( nFound->se == 0 ) continue;
+			mini = nFound->fi;
+			break;
+		}
+		if ( !isExist) {
 			cout << "EMPTY" << endl;
 			continue;
 		}
-		cout << res1 << ' ' << res2 << endl;
+		cout << maxi << ' ' << mini << endl;
 	}
 
     return 0;
